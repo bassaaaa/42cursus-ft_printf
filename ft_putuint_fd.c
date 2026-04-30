61b4dbd16ffe3f8c279a5147660f96f694058375
bassaaaa <white.galaxys2.album2@gmail.com>
2026-04-30 21:31:30 +0900

    フォーマットを適用

--- ft_putuint_fd.c
+++ ft_putuint_fd.c
@@ -6,15 +6,16 @@
 /*   By: tsito <tsito@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/30 21:25:48 by tsito             #+#    #+#             */
-/*   Updated: 2026/04/30 21:29:47 by tsito            ###   ########.fr       */
+/*   Updated: 2026/04/30 21:31:13 by tsito            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 
-void ft_putuint_fd(unsigned int n, int fd) {
-	char			buf[10];
-	char			*pos;
+void	ft_putuint_fd(unsigned int n, int fd)
+{
+	char	buf[10];
+	char	*pos;
 
 	pos = &buf[10];
 	if (n == 0)
